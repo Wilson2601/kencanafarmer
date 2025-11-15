39012dca59db9d36f0f2e9c05a779ba54f023847
111liew <bcs24020052@student.uts.edu.my>
2025-11-15 11:34:38 +0800

    Add HarvestPrediction component as a placeholder

---
+++ js/components/HarvestPrediction.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function HarvestPrediction() {
+  return (
+    <div className="container">
+      <h1>Harvest Prediction (placeholder)</h1>
+    </div>
+  );
+}
